6060a62594e73d31f8e0b9faa9b979713b22fa74
Kasper Munch <kaspermunch@birc.au.dk>
2025-02-08 19:54:09 +0100

    Update docs-run-notebooks.sh

--- docs-run-notebooks.sh
+++ docs-run-notebooks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 for FILE in docs/pages/*.ipynb ; do
+    echo -e "${BLUE}Rendering ${FILE}${NC} ..."
     jupyter nbconvert --to notebook --execute --inplace $FILE || exit 1 ;
 done
