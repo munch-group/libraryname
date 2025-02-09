b9011019ba47c8093834c044236c77490dc45f73
Kasper Munch <kaspermunch@birc.au.dk>
2025-02-09 10:02:04 +0100

    Update docs-run-notebooks.sh

--- docs-run-notebooks.sh
+++ docs-run-notebooks.sh
@@ -4,6 +4,8 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 for FILE in docs/pages/*.ipynb ; do
-    echo -e "${BLUE}Rendering ${FILE}${NC} ..."
-    jupyter nbconvert --to notebook --execute --inplace $FILE || exit 1 ;
-done
+    echo -e "${BLUE}Rendering ${FILE}${NC}"
+    CMD="jupyter nbconvert --Application.log_level=50 --to notebook --execute --inplace $FILE"
+    echo $CMD
+    PYDEVD_DISABLE_FILE_VALIDATION=1 $CMD || exit 1 ;
+ done
