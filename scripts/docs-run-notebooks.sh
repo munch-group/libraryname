7717ea07dd28afd7cea74fbf37483aae8fe9f833
Kasper Munch <kaspermunch@birc.au.dk>
2025-07-25 11:47:00 +0200

    Added files from projectname to library name

--- scripts/docs-run-notebooks.sh
+++ scripts/docs-run-notebooks.sh
@@ -3,6 +3,11 @@
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+if test -d ./docs/pages; then
+  echo "docs/pages directory does not exist"
+  exit(1)
+fi
+
 for FILE in docs/pages/*.ipynb ; do
     echo -e "${BLUE}Rendering ${FILE}${NC}"
     CMD="jupyter nbconvert --Application.log_level=50 --to notebook --execute --inplace $FILE"
